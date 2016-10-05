30c8b504bb6ff8babb7491e882e6c3dee347459c
pikesley <github@orgraphone.org>
2016-10-05 22:24:06 +0100

    Shitting hell it works

--- lib/wen/clock/clock.rb
+++ lib/wen/clock/clock.rb
@@ -48,6 +48,7 @@ module Wen
     end
 
     def self.colours params
+      puts params
       params.each_pair do |wheel, values|
         values.each_pair do |layer, colour|
           $redis.set "#{wheel}/#{layer}", colour.join(', ')
