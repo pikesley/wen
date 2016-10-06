0a3c71f04a0b03a06c4c99bf92bb836c20d52efc
pikesley <github@orgraphone.org>
2016-10-06 18:20:28 +0100

    Better tricks

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
 require File.join(File.dirname(__FILE__), 'lib/wen.rb')
 Wen.stash_colours
-Wen::Clock::Tricks.boot_up
+Wen::Clock::Tricks.boot_up if RUBY_PLATFORM =~ /arm.*-linux/
 run Wen::App
--- lib/wen/clock/tricks.rb
+++ lib/wen/clock/tricks.rb
@@ -14,8 +14,13 @@ module Wen
       end
 
       def self.blink duration = 1
-        self.one_colour Clock.fetch_colour 'minutes', 'face'
-        sleep duration unless ENV['environment'] == 'test'
+        self.clear
+        sleep duration / 10.0
+        self.one_colour Clock.fetch_colour 'minutes', 'hands'
+        sleep duration if RUBY_PLATFORM =~ /arm.*-linux/
+        self.clear
+        sleep duration / 10.0
+        
         Clock.time
       end
 
@@ -26,7 +31,8 @@ module Wen
           Timecop.freeze DateTime.strptime(now.to_s, '%s') do
             Clock.time
             now += 3930
-            sleep 0.05 unless ENV['environment'] == 'test'
+            sleep 0.05 if RUBY_PLATFORM =~ /arm.*-linux/
+
           end
         end
 
@@ -45,7 +51,8 @@ module Wen
       def self.shuffle iterations = 64
         iterations.times do
           Clock.time DateTime.strptime(Random.rand(86400).to_s, '%s')
-          sleep 0.05 unless ENV['environment'] == 'test'
+          sleep 0.05 if RUBY_PLATFORM =~ /arm.*-linux/
+
         end
 
         Clock.time
@@ -53,20 +60,23 @@ module Wen
 
       def self.boot_up
         self.clear
-        sleep 1 unless ENV['environment'] == 'test'
+        sleep 1 if RUBY_PLATFORM =~ /arm.*-linux/
+
 
         a = []
         Config.instance.config.neopixels['minutes']['pins'].times do |i|
           a.push Config.instance.config['colours']['red']
           Neopixels.instance.illuminate a
-          sleep 0.5 unless ENV['environment'] == 'test'
+          sleep 0.5 if RUBY_PLATFORM =~ /arm.*-linux/
+
         end
 
         Config.instance.config.neopixels['hours']['pins'].times do |i|
           a.push Config.instance.config['colours']['blue']
         end
         Neopixels.instance.illuminate a
-        sleep 2 unless ENV['environment'] == 'test'
+        sleep 2 if RUBY_PLATFORM =~ /arm.*-linux/
+
 
         Clock.time
       end
