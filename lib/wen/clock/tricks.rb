b0867fe23ccd9b10b5b5826c740e6fabc2ba32f3
pikesley <github@orgraphone.org>
2016-10-14 10:45:22 +0100

    Wipe works

--- lib/wen/clock/tricks.rb
+++ lib/wen/clock/tricks.rb
@@ -140,7 +140,7 @@ module Wen
           TOTAL_LENGTH.times do |i|
             a[i] = Config.instance.config.colours[key]
             Neopixels.instance.illuminate a
-            sleep 75 / 1000.0
+            sleep 50 / 1000.0
           end
         end
 
