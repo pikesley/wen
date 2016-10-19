0365cc9e3644b9ba4ed7b3ce434fe2b214abd0aa
pikesley <github@orgraphone.org>
2016-10-19 14:45:41 +0100

    New scripts

--- scripts/hit-clock.sh
+++ scripts/hit-clock.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-while [ 1 ]
-do
-  echo `date` >> /tmp/hit-clock
+date >> /tmp/date
 
-  curl --request POST \
-       --silent \
-       --header "Content-Type:application/json" \
-       --header "Accept: application/json" \
-       http://localhost:8080/time
-
-  sleep 10s
-done
+curl --request POST \
+     --silent \
+     --header "Content-Type:application/json" \
+     --header "Accept: application/json" \
+     http://localhost:8080/time
