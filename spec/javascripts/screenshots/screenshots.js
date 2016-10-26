5049ffd2d862d0ee60a6ff1109b815c1feda20a0
pikesley <github@orgraphone.org>
2016-10-26 21:11:47 +0100

    Take screenshots

---
+++ spec/javascripts/screenshots/screenshots.js
@@ -0,0 +1,31 @@
+module.exports = {
+  'Colours screenshot': function(browser) {
+    browser
+      .url('http://localhost:9292/colours')
+      .waitForElementVisible('body', 1000)
+      .resizeWindow(400, 700)
+      .click('a[id=reset]')
+      .click('#hours-hand')
+      .waitForElementVisible('.hours-hand-picker', 1000)
+      .saveScreenshot('./screenshot/colours.png')
+      .end()
+  },
+
+  'Modes screenshot': function(browser) {
+    browser
+      .url('http://localhost:9292/modes')
+      .waitForElementVisible('body', 1000)
+      .resizeWindow(400, 700)
+      .saveScreenshot('./screenshot/modes.png')
+      .end()
+  },
+
+  'Tricks screenshot': function(browser) {
+    browser
+      .url('http://localhost:9292/tricks')
+      .waitForElementVisible('body', 1000)
+      .resizeWindow(400, 700)
+      .saveScreenshot('./screenshot/tricks.png')
+      .end()
+  }
+}
