5714ecbc53f0eb54a5d4f9bd8cd50d16ebd59f47
pikesley <github@orgraphone.org>
2016-10-27 22:46:40 +0100

    Nicer mode picker

--- spec/javascripts/nightwatch/modes_spec.js
+++ spec/javascripts/nightwatch/modes_spec.js
@@ -6,13 +6,25 @@ module.exports = {
       .url('http://localhost:9292/modes')
       .waitForElementVisible('body', timeout)
       .assert.containsText('h1', 'modes')
-      .click('button[id=vague-mode]')
-      .click('button[id=about-vague-mode]')
-      .waitForElementVisible('#vague-modal', timeout)
+
+      // select vague mode
+      .click('#vague-mode-picker')
+
+      // button becomes active
       .assert.cssClassPresent('#about-vague-mode', 'btn-active')
+
+      // open vague modal
+      .click('#about-vague-mode')
+      .waitForElementVisible('#vague-modal', timeout)
+
+      // only this one has the text
       .assert.containsText('#vague-is-current', 'this mode is currently selected')
       .assert.hidden('#range-is-current')
       .assert.hidden('#strict-is-current')
+
+      // and we should not see this button
+      .assert.hidden('#vague-mode-modal-picker')
+
       .end()
   }
 }
