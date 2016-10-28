c10eea1c1320d3e3318471c8c9d7021f56c2c08b
pikesley <github@orgraphone.org>
2016-10-28 15:40:29 +0100

    Queues have priorities

--- lib/wen/clock_workers.rb
+++ lib/wen/clock_workers.rb
@@ -2,8 +2,12 @@ module Wen
   class ClockWorker
     include Sidekiq::Worker
 
-    def self.report params = 'none'
-      puts "#{self.name}: #{params}" unless ($is_pi || ENV['RACK_ENV'] == 'test')
+    def self.commence params = 'none'
+      puts "Starting #{self.name}: #{params}" unless ENV['RACK_ENV'] == 'test'
+    end
+
+    def self.cease params = 'none'
+      puts "Finishing #{self.name}: #{params}" unless ENV['RACK_ENV'] == 'test'
     end
   end
 
@@ -11,13 +15,13 @@ module Wen
     sidekiq_options queue: 'colours'
 
     def perform params
-      self.class.report params
-
+      self.class.commence params
       if params == 'reset'
         Wen.stash_colours
       else
         Clock.send(:colours, params)
       end
+      self.class.cease params
     end
   end
 
@@ -25,9 +29,9 @@ module Wen
     sidekiq_options queue: 'modes'
 
     def perform params
-      self.class.report params
-
+      self.class.commence params
       Clock.mode = params['mode']
+      self.class.cease params
     end
   end
 
@@ -35,9 +39,9 @@ module Wen
     sidekiq_options queue: 'tricks'
 
     def perform params
-      self.class.report params
-
+      self.class.commence params
       Clock::Tricks.send(params['trick'].to_sym)
+      self.class.cease params
     end
   end
 
@@ -45,9 +49,9 @@ module Wen
     sidekiq_options queue: 'time'
 
     def perform
-      self.class.report
-
+      self.class.commence
       Clock.time
+      self.class.cease 
     end
   end
 end
--- lib/wen/helpers.rb
+++ lib/wen/helpers.rb
@@ -4,8 +4,6 @@ if ENV['RACK_ENV'] == 'test'
   $is_pi = false
 end
 
-$is_pi = true
-
 module Wen
   module Helpers
   end
