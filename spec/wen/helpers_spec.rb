2df21411290051380602496c9ee77774d84312dd
pikesley <github@orgraphone.org>
2016-11-07 19:07:54 +0000

    Swipes!

--- spec/wen/helpers_spec.rb
+++ spec/wen/helpers_spec.rb
@@ -13,5 +13,28 @@ module Wen
     it 'titlecases a string' do
       expect(helpers.titlecase 'clock modes').to eq 'Clock Modes'
     end
+
+    context 'next and previous' do
+      it 'works for the easy case' do
+        expect(helpers.next_and_previous 'tricks').to eq ({
+          next: 'about',
+          previous: 'modes'
+        })
+      end
+
+      it 'handles the low-end case' do
+        expect(helpers.next_and_previous 'colours').to eq ({
+          next: 'modes',
+          previous: nil
+        })
+      end
+
+      it 'handles the top-end case' do
+        expect(helpers.next_and_previous 'about').to eq ({
+          next: nil,
+          previous: 'tricks'
+        })
+      end
+    end
   end
 end
